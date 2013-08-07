659e8de22ab665cbaa799c2ab10aafbe3b7c13ac
Nevo <sakur.deagod@gmail.com>
2013-08-07 17:34:33 +1000

    Add testKeyExchange1 and testKeyExchange2 for testing DH key exchange algo.

--- dhkxTests/dhkxTests.m
+++ dhkxTests/dhkxTests.m
@@ -108,6 +108,62 @@
 
 /*
  * Test Subject:
+ *   DH Key Exchange Algorithm
+ */
+
+- (void)testKeyExchange1
+{
+    DHGroup *group = [DHGroup groupWithGroupId:DHGROUP_FOURTEEN];
+    STAssertTrue(group.p != NULL, @"A safe prime is required");
+    STAssertTrue(group.g != NULL, @"A generator is required");
+
+    DHKey *p1 = [[DHKey alloc] initWithDHGroup:group];
+    STAssertTrue(p1.publicKey != nil, @"p1 has nil DH pub key");
+    DHKey *p2 = [[DHKey alloc] initWithDHGroup:group];
+    STAssertTrue(p2.publicKey != nil, @"p2 has nil DH pub key");
+
+    NSData *s1 = [p1 computeSecretWithPublicKey:p2.publicKey];
+    STAssertTrue(s1 != nil, @"p1 failed to compute shared secret");
+    NSData *s2 = [p2 computeSecretWithPublicKey:p1.publicKey];
+    STAssertTrue(s2 != nil, @"p2 failed to compute shared secret");
+
+    STAssertTrue([s1 isEqualToData:s2], @"s1 and s2 are not equal");
+
+    [p1 release];
+    [p2 release];
+}
+
+
+- (void)testKeyExchange2
+{
+    int ret;
+    ret = BN_hex2bn(&lP, "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A63A36210000000000090563");
+    STAssertTrue(ret > 0, @"BN_hex2bn -> p failure");
+    ret = BN_set_word(lG, 2);
+    STAssertTrue(ret > 0, @"BN_set_word -> g failure");
+
+    DHGroup *group = [DHGroup groupWithPrime:lP
+                                    generate:lG];
+    
+    DHKey *p1 = [[DHKey alloc] initWithDHGroup:group];
+    STAssertTrue(p1.publicKey != nil, @"p1 has nil DH pub key");
+    DHKey *p2 = [[DHKey alloc] initWithDHGroup:group];
+    STAssertTrue(p2.publicKey != nil, @"p2 has nil DH pub key");
+    
+    NSData *s1 = [p1 computeSecretWithPublicKey:p2.publicKey];
+    STAssertTrue(s1 != nil, @"p1 failed to compute shared secret");
+    NSData *s2 = [p2 computeSecretWithPublicKey:p1.publicKey];
+    STAssertTrue(s2 != nil, @"p2 failed to compute shared secret");
+    
+    STAssertTrue([s1 isEqualToData:s2], @"s1 and s2 are not equal");
+    
+    [p1 release];
+    [p2 release];
+}
+
+
+/*
+ * Test Subject:
  *   NIST FCC DH static key validation scheme
  *
  * The test vector is from:
